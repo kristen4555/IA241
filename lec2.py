dad14c3878bbf90af52533ded961bec7658069ca
EC2 Default User <ec2-user@ip-172-31-59-96.ec2.internal>
2022-01-26 21:39:58 +0000

    lec2

---
+++ lec2.py
@@ -0,0 +1,20 @@
+"""
+this is a regional comment
+"""
+print('hello world') # this is a line comment
+print(123)
+print(123.)
+print('123')
+
+print(123)
+
+print(123.)
+
+print('123')
+
+print( 123 )
+
+print( 123. )
+
+print(type() '123' )
+
