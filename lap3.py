a3f36f132546d184d5dccd0e127708a05d9daaf9
EC2 Default User <ec2-user@ip-172-31-59-96.ec2.internal>
2022-02-07 21:28:16 +0000

    my lec3.py file.

---
+++ lap3.py
@@ -0,0 +1,24 @@
+'''
+lab3
+list and set
+'''
+#3.1
+str_list = [ 'a', 'd', 'e', 'b', 'c']
+
+str_list.sort()
+
+print(str_list)
+
+#3.2
+
+str_list.append('f')
+print(str_list)
+
+#3.3
+
+str_list.remove('d')
+print(str_list)
+
+#3.4
+
+print(str_list[2])
