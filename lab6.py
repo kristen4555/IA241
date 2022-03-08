c9585460639df1c6a54a4f56444203bc4d23e56f
EC2 Default User <ec2-user@ip-172-31-59-96.ec2.internal>
2022-03-08 00:16:24 +0000

    lab6

---
+++ lab6.py
@@ -0,0 +1,46 @@
+'''
+lab6
+'''
+
+#3.1
+
+for i in range(6):
+    
+    if i !=3:
+        print(i)
+        
+#3.2
+
+result = 1 
+
+for i in range(1,6):
+    
+        print(i)
+        
+        result = result * i
+        
+print(result)
+
+#3.3
+
+result = 0
+
+for i in range(1,6):
+    
+        print(i)
+        
+        result = result + i
+        
+print(result)
+
+#3.4
+
+result = 0
+
+for i in range(3,9):
+    
+        print(i)
+        
+        result = result * i
+        
+print(result)
---
+++ lec6.py
@@ -0,0 +1,29 @@
+'''
+lec6
+'''
+
+for letter in ['a', 'b', 'c']:
+    
+        print(letter)
+        
+demo_str = 'this is my string'
+
+for c in demo_str:
+    
+        print(c)
+        
+for word in demo_str.split():
+            
+        print(word)
+        
+num_list = [1,2,3,1]
+ 
+max_item = num_list[0]
+ 
+for num in num_list:
+     
+     if max_item<= num:
+        max_item = 1
+        
+print(max_item)
+
